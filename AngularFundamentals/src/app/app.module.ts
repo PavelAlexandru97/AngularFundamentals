4da635c7dce611c9839a647580d9f1c5c63aaccb
pavel.bajenaru <Pavel.Bajenaru@iquestgroup.com>
2019-10-15 16:32:18 +0300

    master: Get in started

---
+++ AngularFundamentals/src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { EventsListComponent } from './events/events-list-component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { EventsAppComponent } from './events-app.component';
+
+@NgModule({
+  declarations: [EventsAppComponent, EventsListComponent],
+  imports: [BrowserModule],
+  providers: [],
+  bootstrap: [EventsAppComponent]
+})
+export class AppModule {}
