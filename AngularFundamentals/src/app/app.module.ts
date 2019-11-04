ce8d56e1775d802b95dbe55b196e36ecffb3cad6
pavel.bajenaru <Pavel.Bajenaru@iquestgroup.com>
2019-11-04 14:10:54 +0200

    [develop]: Install toastr

--- AngularFundamentals/src/app/app.module.ts
+++ AngularFundamentals/src/app/app.module.ts
@@ -1,14 +1,16 @@
+import { ToastrService } from './common/toastr.ervice';
 import { EventsAppComponent } from './events-app.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { EventsListComponent } from './events/events-list.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NavBarComponent } from './nav/navbar.component';
+import { EventService } from './events/shared/event.service';
 
 @NgModule({
   declarations: [EventsListComponent, EventThumbnailComponent, NavBarComponent, EventsAppComponent],
   imports: [BrowserModule],
-  providers: [],
+  providers: [EventService, ToastrService],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule {}
--- AngularFundamentals/src/app/events/event-thumbnail.component.ts
+++ AngularFundamentals/src/app/events/event-thumbnail.component.ts
@@ -4,13 +4,19 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   selector: 'event-thumbnail',
   template: `
     <div class="well hoverwell thumbnail">
-      <h2>{{ event.name }}</h2>
-      <div>Date:{{ event.date }}</div>
-      <div>Price: \${{ event.price }}</div>
-      <div>
-        <span> Location: {{ event.location.address }}</span>
+      <h2>{{ event?.name }}</h2>
+      <div>Date:{{ event?.date }}</div>
+      <div [ngSwitch]="event?.time">
+        Time:{{ event?.time }}
+        <span *ngSwitchCase="'8:00 am'">(Eatly Start)</span>
+        <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+        <span *ngSwitchDefault>(Normal Start)</span>
+      </div>
+      <div>Price: \${{ event?.price }}</div>
+      <div *ngIf="event?.location.address">
+        <span> Location: {{ event?.location?.address }}</span>
         <span>&nbsp;</span>
-        <span>{{ event.location.city }}, {{ event.location.country }}</span>
+        <span>{{ event?.location.city }}, {{ event?.location?.country }}</span>
       </div>
     </div>
   `,
