5d129364a4cb57785405fa76c5a6482a84768562
pavel.bajenaru <Pavel.Bajenaru@iquestgroup.com>
2019-11-04 09:03:30 +0200

    New branch (develop)

--- AngularFundamentals/src/app/app.module.ts
+++ AngularFundamentals/src/app/app.module.ts
@@ -1,11 +1,12 @@
-import { EventsListComponent } from './events/events-list-component';
+import { EventsAppComponent } from './events-app.component';
+import { EventThumbnailComponent } from './events/event-thumbnail.component';
+import { EventsListComponent } from './events/events-list.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { EventsAppComponent } from './events-app.component';
+import { NavBarComponent } from './nav/navbar.component';
 
 @NgModule({
-  declarations: [EventsAppComponent, EventsListComponent],
+  declarations: [EventsListComponent, EventThumbnailComponent, NavBarComponent, EventsAppComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [EventsAppComponent]
